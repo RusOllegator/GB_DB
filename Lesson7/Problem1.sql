44a7a1044c5cf026b445b4d613f8e071f7b373f5
Oleg Skorobogaty <oskorobogaty@gmail.com>
2020-05-26 15:53:39 +0300

    Вторая задача

---
+++ Lesson7/Problem1.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE Lesson7;
+USE Lesson7;
+
+-- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+-- select * from users;
+ 
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+) COMMENT = 'Заказы';
+
+
+INSERT INTO orders (user_id) VALUES
+  (1),(2),(1),(4),(5),(1),(4);
+  
+SELECT DISTINCT u.name FROM 
+	users u	
+	JOIN 
+	orders o 
+	ON u.id = o.user_id;
+
+ 
